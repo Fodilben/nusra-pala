7203b781741206c99a89877f975d4e253ea8f5e0
fodil benhadjer <109416421+Fodilben@users.noreply.github.com>
2024-03-10 15:43:16 +0000

    i dont know

--- src/App.jsx
+++ src/App.jsx
@@ -24,29 +24,27 @@ const App = () => {
         messages={resources[language]}
       ></IntlProvider>
       <main>
-      <section>
-        <Header />
-      </section>
-      <section>
-        <Hero />
-      </section>
-      <section>
-        <AboutUs />
-      </section>
-      <section>
-        <Goal />
-      </section>
-      <section>
-        <Contact />
-      </section>
-      <section>
-        <Footer />
-      </section>
-    </main>
+        <section>
+          <Header />
+        </section>
+        <section>
+          <Hero />
+        </section>
+        <section>
+          <AboutUs />
+        </section>
+        <section>
+          <Goal />
+        </section>
+        <section>
+          <Contact />
+        </section>
+        <section>
+          <Footer />
+        </section>
+      </main>
     </div>
   );
 };
 
-
-
-
+export default App;
